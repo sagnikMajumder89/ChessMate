3f23deba7111fb9b28536c1469dd8f4edd23ca57
Sagnik Majumder <sagnikm183@gmail.com>
2025-05-28 13:05:23 +0530

    fixed minor issue

--- components/playBot/BotComponent/Chessboard.tsx
+++ components/playBot/BotComponent/Chessboard.tsx
@@ -14,6 +14,7 @@ import {
 import { Button } from "@/components/ui/button";
 import PlayerDetails from "../Dialogs/PlayerDetails";
 import { Move } from "@/lib/interfaces";
+import { set } from "react-hook-form";
 
 type Data = {
   color: string;
@@ -161,6 +162,7 @@ export default function BotChessboard({
 
         if (result) {
           setFen(gameRef.current.fen());
+          setLastMove({ from, to });
           checkGameOver();
         } else {
           toast.error("Invalid bot move");
